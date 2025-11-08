74227de06475b6460b9ebd43acb7c9507a06d5e4
Caio Barbosa Galvão <caio_bgalvao@hotmail.com>
2025-11-08 20:50:30 -0300

    feat(personagem): tomarDano function implemented

--- Orientada a Objeto/Caio/personagem.py
+++ Orientada a Objeto/Caio/personagem.py
@@ -3,3 +3,9 @@ class Personagem:
         self.__nome = nome
         self.__pontosVida = 100
         self.__pontosMana = 50
+
+    def tomarDano(self, dano: int):
+        if dano <= self.__pontosVida:
+            self.__pontosVida = 0
+        else:
+            self.__pontosVida -= dano
