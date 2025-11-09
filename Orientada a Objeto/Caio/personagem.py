07cd1c682d1bb11a15725151567c0b72e4019193
Caio Barbosa Galvão <caio_bgalvao@hotmail.com>
2025-11-08 21:04:45 -0300

    feat(personagem): branchless tomarDano implemented

--- Orientada a Objeto/Caio/personagem.py
+++ Orientada a Objeto/Caio/personagem.py
@@ -5,10 +5,8 @@ class Personagem:
         self.__pontosMana = 50
 
     def tomarDano(self, dano: int):
-        if dano >= self.__pontosVida:
-            self.__pontosVida = 0
-        else:
-            self.__pontosVida -= dano
+        nova_vida = self.__pontosVida - dano
+        self.__pontosVida = max(nova_vida, 0)
 
     def curar(self, cura: int):
         nova_vida = self.__pontosVida + cura
